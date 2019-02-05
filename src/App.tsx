4a623f015c01f8336803f48cfa39da358a16e5b0
Lucas Silva <lucas.silva@avecbrasil.com.br>
2019-02-05 15:11:57 -0200

    Criado componente de mostrar os horarios  no qual precisa-se percorrer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import styled, { keyframes } from "styled-components";
+import { number } from "prop-types";
 
 const Container = styled("div")<{ height: number }>`
   width: 100%;
   height: ${props => props.height}px;
-  background-color: palevioletred;
+  display: flex;
 `;
 
 const Orb = styled.div`
@@ -28,33 +29,88 @@ const gravitation = (ride: number, currentTime: number) => keyframes`
   to {
     transform: translateY(${ride}px);
   }
-  `
-;
+  `;
 
 /**
- * 
+ *
  * O tempo total do está em minutos, então multiplicamos por 60 porque no cenário real ele deve se movimentar em segundos
-*/
-const Trace = styled("div")<{ ride: number; totalTime: number; currentTime: number }>`
+ */
+const Trace = styled("div")<{
+  ride: number;
+  totalTime: number;
+  currentTime: number;
+}>`
   flex-direction: row;
   height: 8px;
   display: flex;
   align-items: center;
-  animation: ${props => gravitation(props.ride, props.currentTime)} ${props => (props.totalTime * 60)}s linear;
+  width: 100%;
+  animation: ${props => gravitation(props.ride, props.currentTime)}
+    ${props => props.totalTime * 60}s linear;
 `;
+
+/**
+ * Component HoursLane
+ */
+
+interface IMetricHours {
+  initial: number;
+  end: number;
+  hourHeight: number;
+  totalHeight: number;
+}
+
+const HoursLane = styled("div")<{ height: number }>`
+  flex-direction: coloumn;
+  width: 36px;
+  height: ${props => props.height}px;
+`;
+
+const Hour = styled("div")<{
+  hourHeight: number;
+}>`
+  height: ${props => props.hourHeight}px;
+  display: flex;
+  justify-content: center;
+  font-size: 14px;
+  color: #7b7b88;
+`;
+
+export const MetricHours = (props: IMetricHours) => {
+  return (
+    <HoursLane height={props.totalHeight}>
+      <Hour hourHeight={props.hourHeight}>8:00</Hour>
+      <Hour hourHeight={props.hourHeight}>9:00</Hour>
+      <Hour hourHeight={props.hourHeight}>10:00</Hour>
+      <Hour hourHeight={props.hourHeight}>11:00</Hour>
+      <Hour hourHeight={props.hourHeight}>12:00</Hour>
+      <Hour hourHeight={props.hourHeight}>13:00</Hour>
+      <Hour hourHeight={props.hourHeight}>14:00</Hour>
+      <Hour hourHeight={props.hourHeight}>15:00</Hour>
+      <Hour hourHeight={props.hourHeight}>16:00</Hour>
+      <Hour hourHeight={props.hourHeight}>17:00</Hour>
+      <Hour hourHeight={props.hourHeight}>18:00</Hour>
+      <Hour hourHeight={props.hourHeight}>19:00</Hour>
+      <Hour hourHeight={props.hourHeight}>20:00</Hour>
+      <Hour hourHeight={props.hourHeight}>21:00</Hour>
+    </HoursLane>
+  );
+};
 
 export default class App extends Component {
   state = {
     initialJourney: 480,
     finalJourney: 1320,
     pixelPerHour: 90,
+    hoursOpenByDay: 0,
     totalPixelsLane: 0,
     totalTime: 0, // in minutes
-    currentTime: 0, // in minutes
+    currentTime: 0 // in minutes
   };
 
   componentDidMount() {
-    const hoursOpenByDay = (this.state.finalJourney - this.state.initialJourney) / 60;
+    const hoursOpenByDay =
+      (this.state.finalJourney - this.state.initialJourney) / 60;
     const totalPixelsLane = this.state.pixelPerHour * hoursOpenByDay;
     const dropSpeed = this.state.pixelPerHour / 60; // pixels/min
     const totalTime = totalPixelsLane / dropSpeed;
@@ -64,16 +120,33 @@ export default class App extends Component {
      */
     const now = new Date();
     const currentTime = now.getHours() * 60 + now.getMinutes();
-
-    this.setState({ totalPixelsLane, totalTime, currentTime });
+    console.log('Current time ==>>', currentTime);
+    this.setState({ totalPixelsLane, totalTime, currentTime, hoursOpenByDay });
   }
 
   render() {
-    const { totalPixelsLane, totalTime, currentTime } = this.state;
+    const {
+      totalPixelsLane,
+      totalTime,
+      currentTime,
+      pixelPerHour,
+      finalJourney,
+      initialJourney
+    } = this.state;
 
     return (
       <Container height={totalPixelsLane}>
-        <Trace ride={totalPixelsLane} totalTime={totalTime} currentTime={currentTime}>
+        <MetricHours
+          initial={initialJourney}
+          end={finalJourney}
+          hourHeight={pixelPerHour}
+          totalHeight={totalPixelsLane}
+        />
+        <Trace
+          ride={totalPixelsLane}
+          totalTime={totalTime}
+          currentTime={currentTime}
+        >
           <Orb />
           <Line />
         </Trace>
